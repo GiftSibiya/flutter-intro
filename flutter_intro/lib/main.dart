acd26449bf81b6cb80bb05c6ad892640dcc5910f
Gift Sibiya <144510689+GiftSibiya@users.noreply.github.com>
2023-10-18 16:20:38 +0200

    updated ui

--- flutter_intro/lib/main.dart
+++ flutter_intro/lib/main.dart
@@ -1,8 +1,22 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(Text(
-    "Hello World",
-    textDirection: TextDirection.ltr,
+  runApp(MaterialApp(
+    title: "Dojo 1",
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text("Glory to God"),
+        backgroundColor: Color.fromARGB(255, 33, 198, 115),
+      ),
+      body: Center(
+        child: Text.rich(TextSpan(text: "my", children: [
+          TextSpan(
+              text: "Flutter",
+              style: TextStyle(fontSize: 50.0, fontWeight: FontWeight.bold)),
+          TextSpan(
+              text: "App", style: TextStyle(fontSize: 30.0, color: Colors.blue))
+        ])),
+      ),
+    ),
   ));
 }
