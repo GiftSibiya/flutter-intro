5ad8a3c23400379dcd3e1aa762056aec0dbb8d47
GiftSibiya <giftsibiya.gb@gmail.com>
2023-10-19 21:41:30 +0200

    updated build context

--- flutter_intro/lib/main.dart
+++ flutter_intro/lib/main.dart
@@ -1,22 +1,25 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    title: "Dojo 1",
-    home: Scaffold(
-      appBar: AppBar(
-        title: Text("Glory to God"),
-        backgroundColor: Color.fromARGB(255, 33, 198, 115),
-      ),
-      body: Center(
-        child: Text.rich(TextSpan(text: "my", children: [
-          TextSpan(
-              text: "Flutter",
-              style: TextStyle(fontSize: 50.0, fontWeight: FontWeight.bold)),
-          TextSpan(
-              text: "App", style: TextStyle(fontSize: 30.0, color: Colors.blue))
-        ])),
-      ),
-    ),
-  ));
+  runApp(taxiMath());
+}
+
+class taxiMath extends StatelessWidget {
+  const taxiMath({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Front Seat",
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text("Taxi Math"),
+            backgroundColor: Colors.blue,
+            centerTitle: true,
+          ),
+          body: Center(
+            child: Text("Hello world"),
+          )),
+    );
+  }
 }
