3304723c52992987ec8f98571838208ee8996659
GiftSibiya <giftsibiya.gb@gmail.com>
2023-10-21 21:59:46 +0200

    added list view columns

--- flutter_intro/lib/main.dart
+++ flutter_intro/lib/main.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: camel_case_types
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,16 +12,82 @@ class taxiMath extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Front Seat",
+      title: "Taxi Math",
       home: Scaffold(
-          appBar: AppBar(
-            title: Text("Taxi Math"),
-            backgroundColor: Colors.blue,
-            centerTitle: true,
-          ),
-          body: Center(
-            child: Text("Hello world"),
-          )),
+        appBar: AppBar(
+          title: Text("Lists"),
+          toolbarHeight: 70,
+        ),
+        body: ListView(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                height: 200,
+                decoration: BoxDecoration(
+                    color: Colors.deepPurple,
+                    borderRadius: BorderRadius.circular(20)),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                height: 200,
+                decoration: BoxDecoration(
+                    border: Border.all(color: Colors.orange, width: 5),
+                    borderRadius: BorderRadius.circular(10)),
+                child: Text("This is in orange"),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                height: 200,
+                color: Colors.red,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                height: 200,
+                color: Colors.deepPurple,
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
+
+class centerContent extends StatefulWidget {
+  const centerContent({super.key});
+
+  @override
+  State<centerContent> createState() => _centerContentState();
+}
+
+class _centerContentState extends State<centerContent> {
+  bool liked = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: [
+        ListTile(
+          title: Text("nice shoes"),
+          trailing: IconButton(
+            icon:
+                liked ? (Icon(Icons.favorite)) : (Icon(Icons.favorite_border)),
+            onPressed: () {
+              setState(() {
+                liked = !liked;
+              });
+            },
+          ),
+        )
+      ],
+    );
+  }
+}
+//Learn listview and and trailing.
